bf0c5b6b3da955e1b795f6aa75af50ea99196f29
jagbum <eric478@naver.com>
2022-11-22 21:58:33 +0900

    프로필

---
+++ app/src/main/java/com/example/yuf2/Setting/DisplayActivity.kt
@@ -0,0 +1,12 @@
+package com.example.yuf2.Setting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.yuf2.R
+
+class DisplayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display)
+    }
+}
