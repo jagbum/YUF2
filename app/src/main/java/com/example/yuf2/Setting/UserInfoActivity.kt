f99c8909c3dc8f56618777b29a68b45905b1fee9
kwon0513 <rnjswls6702@naver.com>
2022-11-24 20:26:43 +0900

    친구프로필확인(미완성)

--- app/src/main/java/com/example/yuf2/Setting/UserInfoActivity.kt
+++ app/src/main/java/com/example/yuf2/Setting/UserInfoActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import com.example.yuf2.R
 
 class UserInfoActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
+
+
+
     }
 }
