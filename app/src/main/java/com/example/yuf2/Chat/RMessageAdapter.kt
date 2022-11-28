5c5c26b6383f4a5f37df56e02eccde7a60a32652
jagbum <102511107+jagbum@users.noreply.github.com>
2022-11-28 21:18:26 +0900

    commit

---
+++ app/src/main/java/com/example/yuf2/Chat/RMessageAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.yuf2.Chat
+
+import android.annotation.SuppressLint
+import android.util.Log
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.yuf2.R
+
+
+class RMessageAdapter(var message_list: java.util.ArrayList<Message>) :
+    RecyclerView.Adapter<RMessageAdapter.MessageHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val itemView: View = inflater.inflate(R.layout.item_rmessage, parent, false)
+        return MessageHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MessageHolder, position: Int) {
+        val temp = message_list[position]
+        holder.setItem(temp)
+    }
+
+    override fun getItemCount(): Int {
+        return message_list.size
+    }
+
+    class MessageHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tv_rmessage_nickname: TextView
+        var tv_rmessage_message: TextView
+        var tv_rmessage_update: TextView
+        var ll_rmessage: LinearLayout
+
+        init {
+            tv_rmessage_nickname = itemView.findViewById<View>(R.id.tv_rmessage_nickname) as TextView
+            tv_rmessage_message = itemView.findViewById<View>(R.id.tv_rmessage_message) as TextView
+            tv_rmessage_update = itemView.findViewById<View>(R.id.tv_rmessage_update) as TextView
+            ll_rmessage = itemView.findViewById<View>(R.id.ll_rmessage) as LinearLayout
+        }
+
+
+        @SuppressLint("ResourceAsColor")
+        fun setItem(item: Message) {
+            tv_rmessage_nickname.text = item.nickname
+            tv_rmessage_message.text = item.msg
+            tv_rmessage_update.text = item.update
+            if (MessageActivity.mynickname.equals(item.nickname)) {
+                ll_rmessage.gravity = Gravity.RIGHT
+                tv_rmessage_message.setBackgroundColor(R.color.custom_realyellow)
+            } else {
+                ll_rmessage.gravity = Gravity.LEFT
+                tv_rmessage_message.setBackgroundColor(R.color.white)
+
+            }
+        }
+    }
+}
